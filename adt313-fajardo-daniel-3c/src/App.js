2aa4d6f55dc225b33b2fc1eb76c6c685290102a1
Daniel Fajardo <151352808+fajardoDaniel@users.noreply.github.com>
2024-11-18 19:01:01 +0800

    Add files via upload
    Update: 
    - Create, Edit and Delete Movies from lists

---
+++ adt313-fajardo-daniel-3c/src/App.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import './index.css';
+import Login from './pages/Public/Login/Login';
+import Dashboard from './pages/Main/Dashboard/Dashboard';
+import Main from './pages/Main/Main';
+import Register2 from './pages/Public/Login/Register2';
+import Movie from './pages/Main/Movie/Movie';
+import Lists from './pages/Main/Movie/Lists/Lists';
+import Form from './pages/Main/Movie/Form/Form';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path:'/register',
+    element:<Register2 />,
+  },
+  {
+    path: '/main',
+    element: <Main />,
+    children: [
+      //{
+        //path: '/main/dashboard',
+        //element: <Dashboard />,
+      //},
+    {
+      path: '/main/movies',
+      element:<Movie />, 
+      children: [
+        {
+          path: '/main/movies',
+          element: <Lists />,
+        },
+        {
+          path: '/main/movies/form/:movieId?',
+          element: <Form />
+        },
+      ],
+    },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className='App'>
+      <RouterProvider router={router} />
+   
+    </div>
+  );
+}
+
+export default App;
